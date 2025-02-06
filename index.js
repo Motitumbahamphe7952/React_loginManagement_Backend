974208d9d52b4d3b56a59a67332aa310e679d18d
Nikhil limbu <nikhillimbu918@gmail.com>
2025-02-06 15:13:49 +0545

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import cors from "cors";
 let expressApp = express();
 
 expressApp.use(cors({
-  origin: ["https://react-login-management-frontend.vercel.app"],
+  origin: [ "https://react-login-management-frontend.vercel.app","https://react-login-management-frontend-fiwu7cf8b.vercel.app"],
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true,
 }));
